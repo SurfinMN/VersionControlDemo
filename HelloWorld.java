48a0a8de90a909b52840677ec83ce11ea39661c3
SurfinMN <162390803+SurfinMN@users.noreply.github.com>
2025-09-29 09:50:57 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+package com.versioncontrol;
+
+/**
+ * Hello World application for M6 T2 Lab Assignment
+ * Demonstrates basic Java programming and version control
+ * 
+ * @author Jeyromy Metho
+ * @version 1.0
+ */
+public class HelloWorld {
+    
+    /**
+     * Main method that prints a greeting message
+     * @param args command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java application for version control demonstration.");
+        System.out.println("Created by: Jeyromy Metho");
+        System.out.println("Course: CIS 175 - Java II");
+        System.out.println("Assignment: M6 T2 Lab - Version Control");
+    }
+}
